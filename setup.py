66251b92b2f20179bbc283ee497818eebec22447
tytusdunin <108412922+tytusdunin@users.noreply.github.com>
2025-11-17 15:57:39 +0100

    Package creation

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@ from distutils.core import setup
 setup(
   name = 'kokosznicka',
   packages = ['kokosznicka'],
-  version = '0.1',
+  version = '0.1.1',
   license='GPL-3.0',
   description = 'A simple and effective syllabificator for the Polish language.',
   author = 'Tytus Dunin',
   author_email = 'tm.dunin@student.uw.edu.pl',
   url = 'https://github.com/tytusdunin/kokosznicka',
-  download_url = 'https://github.com/tytusdunin/kokosznicka/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
-  keywords = ['npl', 'syllabification', 'hyphenation', 'polish'],
-  install_requires=[
-          're',
-      ],
+  download_url = 'https://github.com/tytusdunin/kokosznicka/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['nlp', 'syllabification', 'hyphenation', 'polish'],
+  install_requires=[],
+
   classifiers=[
     'Development Status :: 4 - Beta',
 
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: GPL 3.0 License',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
     'Programming Language :: Python :: 3',  
     'Programming Language :: Python :: 3.4',
