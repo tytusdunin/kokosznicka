1247570ac24bdfe44d91d535164e2f8076002d9e
tytusdunin <108412922+tytusdunin@users.noreply.github.com>
2025-11-17 18:02:14 +0100

    Updating setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'kokosznicka',
   packages = ['kokosznicka'],
-  version = '0.1.2',
+  version = '0.1.3',
   license='GPL-3.0',
   description = 'A simple and effective syllabificator for the Polish language.',
   author = 'Tytus Dunin',
   author_email = 'tm.dunin@student.uw.edu.pl',
   url = 'https://github.com/tytusdunin/kokosznicka',
-  download_url = 'https://github.com/tytusdunin/kokosznicka/archive/refs/tags/v.0.1.2.tar.gz',
+  download_url = 'https://github.com/tytusdunin/kokosznicka/archive/refs/tags/v.0.1.3.tar.gz',
   keywords = ['nlp', 'syllabification', 'hyphenation', 'polish'],
   install_requires=[],
 
